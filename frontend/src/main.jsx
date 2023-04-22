64ec8506925343ad659a2df269461ea7434ffde2
BoazAnicet <boazanicet@ymail.com>
2023-04-22 16:59:01 -0400

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,8 @@ import App from "./App";
 import "./styles/index.scss";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
